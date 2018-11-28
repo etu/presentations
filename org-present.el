82706904f567c63220da4b59167adcd07fdd9db2
Elis Hirwing <elis@hirwing.se>
2018-11-28 18:11:43 +0100

    Update org-present.el to open urls in another browser

--- org-present.el
+++ org-present.el
@@ -1,3 +1,10 @@
+;;; org-present -- Org present tweaks for this presentation
+;;; Commentary:
+;;; Code:
+
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "/nix/var/nix/profiles/system/sw/bin/epiphany")
+
 (eval-after-load "org-present"
   '(progn
      (add-hook 'org-present-mode-hook
@@ -12,3 +19,5 @@
                  (org-remove-inline-images)
                  (org-present-show-cursor)
                  (org-present-read-write)))))
+
+;; org-present.el ends here
