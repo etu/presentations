04fd13a60abedb78d2b86f291befb5f7831e6157
Elis Hirwing <elis@hirwing.se>
2019-07-16 13:55:02 +0200

    build: Reduce the amount of deps required to build pdfs

--- shell.nix
+++ shell.nix
@@ -2,10 +2,17 @@ with (import <nixpkgs> {});
 
 let
   myTexlive = (texlive.combine {
-    inherit (texlive) scheme-medium wrapfig ulem capt-of
-    titlesec preprint enumitem paralist ctex environ
-    beamer trimspaces zhnumber changepage framed
-    fvextra minted upquote ifplatform xstring; });
+    inherit (texlive) scheme-basic
+      # Needed on top of scheme-basic
+      beamer ec etoolbox eurosym translator ulem
+
+      # Needed on top of scheme-small
+      cm-super
+
+      # Needed on top of scheme-medium
+      capt-of wrapfig
+    ;
+  });
 
 in mkShell {
   buildInputs = [
